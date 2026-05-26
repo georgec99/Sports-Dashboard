e6492e59b9230473dcb7713f6cab66b2a3aca9ed
georgec99 <georgetchettle@googlemail.com>
2026-05-26 16:44:10 +0100

    db structure setup

---
+++ db/scorers-tbl.py
@@ -0,0 +1,8 @@
+import sqlite3 as sql
+
+DB_PATH = "sports-dashboard.db"
+
+def getConnection():
+    conn = sql.connect(DB_PATH)
+    conn.row_factory = sql.row  #Access rows by name
+    return conn
